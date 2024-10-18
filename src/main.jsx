0b0005bc5821073ba6a5668aea4de33cf69e5bd1
longgoll <luuhoanglong21@gmail.com>
2024-10-18 13:48:57 +0700

    Refactor main.jsx and add Lad1 component

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import App from './App.jsx'
 import './index.css'
-import Lad2 from './Lad2'
+// import Lad2 from './Lad2'
+import Lad1 from './Lad1'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App/> */}
-    <Lad2 />
+    <Lad1 />
+    {/* <Lad2 /> */}
   </StrictMode>,
 )
